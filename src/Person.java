072c5fbf93cd9cb1dc7decf01641b60fc47e33d9
mustafa <mustafaqasimi8656@gmail.com>
2025-01-06 00:48:23 +0330

    constroctor for person class added

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,10 @@
 public class Person {
+    private String name;
+    private String surname;
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+
 }
