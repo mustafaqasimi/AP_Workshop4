ef2c67fa9189c890ab30564d323611444e5ee1ac
mustafa <mustafaqasimi8656@gmail.com>
2025-01-06 00:51:00 +0330

    Geter and Seter for person class added

--- src/Person.java
+++ src/Person.java
@@ -5,6 +5,19 @@ public class Person {
         this.name = name;
         this.surname = surname;
     }
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
 
 
 }
